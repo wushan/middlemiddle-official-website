86361e03442e41f8a177a8c71f4afb54eee59793
Tony Chiang <chiangloveslynn@gmail.com>
2016-11-19 00:25:30 +0800

    fix ga

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ const scrollBehavior = (to, from, savedPosition) => {
   }
 }
 
-/* eslint-disable no-new */
+/* eslint-disable*/
 const router = new VueRouter({
   mode: 'history',
   base: __dirname,
@@ -53,4 +53,13 @@ new Vue({
   },
   created () {
   }
-}).$mount('#app')
+}).$mount('#app');
+
+// GA
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-68400394-1', 'auto');
+ga('send', 'pageview');
