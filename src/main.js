539788a115b72dbe422496d50454b5108b66b5de
Tony Chiang <chiangloveslynn@gmail.com>
2016-11-19 12:31:36 +0800

    -

--- src/main.js
+++ src/main.js
@@ -63,3 +63,19 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-68400394-1', 'auto');
 ga('send', 'pageview');
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1671181963161506',
+    xfbml      : true,
+    version    : 'v2.5'
+  });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
